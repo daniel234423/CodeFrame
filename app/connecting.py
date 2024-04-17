7b7ac2be6981cbeb5c0db02c6aa9b25aceba3658
daniel234423 <dnulapi@gmail.com>
2024-04-17 09:20:34 -0400

    tratando de poder enviar la foto a la base de datos atraves de el archivo connecting

--- app/connecting.py
+++ app/connecting.py
@@ -1,22 +1,14 @@
 import pymysql
 
 #todo esto dentro de una funcion para despues usar la funcion dentro de las rutas 
-
 connecting = pymysql.connect(host='localhost', user='root', password='Daniel230139', database='codeframe' )
+def conexion(nomber):
+    cursor = connecting.cursor()
+    consulta = "insert into imagenes (img) values (%s)"
+    cursor.execute(consulta,(nomber))
+    connecting.commit()
+    cursor.close()
+    connecting.close()
 
-cursor = connecting.cursor()
-'''
-ruta_img = 'app/static/img/A.png'
-with open(ruta_img, 'rb' ) as f:
-    imagen_blob =  f.read()
-    
-sql = "insert into imagenes (img) values (%s)"
 
-cursor.execute(sql, (imagen_blob,))
-'''
-mostrar = cursor.execute("select img from imagenes")
-print(mostrar)
-connecting.commit()
-cursor.close()
-    
-connecting.close()
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import render_template, Response
 import cv2 as cv
 import imutils as imt
+from flask import render_template, Response, redirect, url_for
 from app.create_app import aplicacion
+from app.connecting import  conexion
 
 app = aplicacion()
 
-cam = cv.VideoCapture(0)  # Cámara de video en la posición 0, que es el dispositivo predeterminado
+foto = cv.VideoCapture(0)  # Cámara de video en la posición 0, que es el dispositivo predeterminado
 
 def obtener_frame():
     while True:
@@ -15,18 +16,31 @@ def obtener_frame():
             yield b"--frame\r\n content-Type: image/jpeg\r\n"+ frame + b"\r\n"
 
 def frames_camara():
-    ret, frame = cam.read()    
+    ret, frame = foto.read()    
+    frame = imt.resize(frame,width=800)
     if not ret:
-                    return False, None
-    _, bufer = cv.imdecode('png', frame)
+        return False, None
+    _, bufer = cv.imencode(".jpg", frame)
     imagen = bufer.tobytes()
     return True, imagen
 
 @app.route("/camara")
 def camara():
     return Response(obtener_frame())
+
+@app.route("/almacenar_foto")
+def almacenar_foto():
+    ret, frame = foto.read()
+    nombre = 1
+    if ret:
+        nombreImg = str(nombre)+".png"
+        cv.imwrite(conexion(nombreImg), frame)
+        return redirect(url_for('index'))
+    return nombre+1
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
